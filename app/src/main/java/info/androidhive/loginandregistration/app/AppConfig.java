0c051b05d4b56f7c160e208af48105d3c283a81b
Bruna Tavares <bruna_satavares@MacBook-Air-de-Bruna.local>
2016-09-30 16:37:40 -0300

    First Login Commit

---
+++ app/src/main/java/info/androidhive/loginandregistration/app/AppConfig.java
@@ -0,0 +1,11 @@
+package info.androidhive.loginandregistration.app;
+
+
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://bzetcardapio.herokuapp.com/login.php";
+	// Server user register url
+	public static String URL_REGISTER = "http://bzetcardapio.herokuapp.com/register.php";
+
+}
